e8e5865a8e56aa082705e70d2227968c8bcffdc8
JJ Kim <jj.dev.001@gmail.com>
2020-07-27 21:24:12 +0900

    Add autoinstall zsh-plugin

--- install.sh
+++ install.sh
@@ -8,3 +8,19 @@ for df in .zshrc .vimrc .tmux.conf; do
     echo symlinked $df to "$HOME"/$df
   fi
 done
+
+
+#
+# install zsh plugins
+#
+autosuggestions="https://github.com/zsh-users/zsh-autosuggestions"
+syntax_highlighting="https://github.com/zsh-users/zsh-syntax-highlighting"
+plugins=($autosuggestions $syntax_highlighting)
+
+for plugin in "${plugins[@]}"; do
+  git -C ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/ clone  $plugin 
+  if [ $? -eq 0 ];then
+    echo "plugin installed: ${plugin}"
+  fi
+done
+
